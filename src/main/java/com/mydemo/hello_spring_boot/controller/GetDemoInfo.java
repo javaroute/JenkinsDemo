9cf725945d3280bb550f8d7827a68207fbface08
e <1586308500@qq.com>
2019-04-28 10:48:15 +0800

    first commit

---
+++ src/main/java/com/mydemo/hello_spring_boot/controller/GetDemoInfo.java
@@ -0,0 +1,13 @@
+package com.mydemo.hello_spring_boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/get")
+public class GetDemoInfo {
+	@RequestMapping("/getone")
+	public void get(){
+		System.out.println("11111111111");
+	}
+}
