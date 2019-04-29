02d8800bf00491139c6467bdd06bd078b5958fe0
e <1586308500@qq.com>
2019-04-29 10:56:57 +0800

    修改

--- src/main/java/com/mydemo/hello_spring_boot/controller/GetDemoInfo.java
+++ src/main/java/com/mydemo/hello_spring_boot/controller/GetDemoInfo.java
@@ -2,11 +2,13 @@ package com.mydemo.hello_spring_boot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/get")
+@RequestMapping("/demo")
 public class GetDemoInfo {
-	@RequestMapping("/getone")
+	@RequestMapping("/jenkins")
+    @ResponseBody
 	public String get(){
 		System.out.println("11111111111");
 		return "SUCCESS";
